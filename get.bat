7396b3f53ab435778e20a80026377b8033bc79c9
madmaxvoltro <altamaxime@gmail.com>
2024-09-19 08:45:13 +0200

    fix

--- get.bat
+++ get.bat
@@ -2,7 +2,7 @@
 
 set "CLONE_DIR=%userprofile%\ReadyOrNot"
 :: Get receiver's IP address
-set RECEIVER_IP=fe80::2829:a852:c3df:71d0%15
+set RECEIVER_IP=10.40.2.23
 
 :: Set shared folder on receiver's PC (replace with receiver's shared folder name)
 set SHARED_FOLDER=SharedFolder
