de1c27eb2d431cbb14d147f1e2cfbc766af42a11
madmaxvoltro <altamaxime@gmail.com>
2024-11-08 10:31:35 +0100

    idk

---
+++ newifne^.bat
@@ -0,0 +1,4 @@
+@echo off
+:: Call the PowerShell script to start listening
+powershell -ExecutionPolicy Bypass -File "C:\Users\mxmla\Documents\ready\Listener.ps1"
+pause
--- one.bat
+++ one.bat
@@ -1,1 +1,10 @@
 @echo off
+:: Set the IP address of the receiver PC
+set receiver_ip=YOUR_RECEIVER_PC_IP
+set port=9999
+
+:: Send a signal to the receiver PC
+echo The PC is ON | powershell -Command "(New-Object System.Net.Sockets.TcpClient).Connect('%receiver_ip%', %port%); $stream = $client.GetStream(); $writer = New-Object System.IO.StreamWriter($stream); $writer.WriteLine('The PC is ON'); $writer.Flush(); $stream.Close(); $client.Close()"
+
+echo Signal sent to your PC. Press any key to exit...
+pause
