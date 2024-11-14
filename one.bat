e549c2552596004e0ec32a7a4102bd0d9a6104f4
madmaxvoltro <altamaxime@gmail.com>
2024-11-14 08:53:52 +0100

    wallpaper

--- one.bat
+++ one.bat
@@ -1,10 +1,26 @@
 @echo off
-:: Set the IP address of the receiver PC
-set receiver_ip=YOUR_RECEIVER_PC_IP
-set port=9999
+setlocal
 
-:: Send a signal to the receiver PC
-echo The PC is ON | powershell -Command "(New-Object System.Net.Sockets.TcpClient).Connect('%receiver_ip%', %port%); $stream = $client.GetStream(); $writer = New-Object System.IO.StreamWriter($stream); $writer.WriteLine('The PC is ON'); $writer.Flush(); $stream.Close(); $client.Close()"
+:: Get the current directory of the batch file
+set "current_dir=%~dp0"
 
-echo Signal sent to your PC. Press any key to exit...
-pause
+:: Set the path to the image file
+set "image_path=%current_dir%r.jpeg"
+
+:: Set the wallpaper using the image path
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%image_path%" /f
+
+:: Refresh the wallpaper settings
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters ,1 ,True
+
+
+@REM @echo off
+@REM :: Set the IP address of the receiver PC
+@REM set receiver_ip=YOUR_RECEIVER_PC_IP
+@REM set port=9999
+
+@REM :: Send a signal to the receiver PC
+@REM echo The PC is ON | powershell -Command "(New-Object System.Net.Sockets.TcpClient).Connect('%receiver_ip%', %port%); $stream = $client.GetStream(); $writer = New-Object System.IO.StreamWriter($stream); $writer.WriteLine('The PC is ON'); $writer.Flush(); $stream.Close(); $client.Close()"
+
+@REM echo Signal sent to your PC. Press any key to exit...
+@REM pause
